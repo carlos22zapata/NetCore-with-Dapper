568239c899549d5862ddf1eeae3a90d7457e53ca
Carlos Zapata <czapata.arg@gmail.com>
2022-08-25 14:59:02 -0300

    my commit

---
+++ ARIS_Net/Controllers/ConnectionsController.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+
+namespace ARIS_Net.Controllers
+{
+    public class ConnectionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Microsoft.AspNetCore.Authorization.Authorize]
+        
+        public string ConBD(string bd)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString = configuration.GetConnectionString(bd);
+                                    
+            return connectionString;
+        }
+
+        [HttpGet]
+        [Route("GetSelect")]
+        public object GetSelect(string ConnectionName, string Command)
+        {
+
+            string connex = ConBD(ConnectionName);
+
+            using (IDbConnection db = new SqlConnection(connex))
+            {
+                db.Open();
+                                
+                try 
+                { 
+                    var result = db.Query(Command).AsList();
+                    return JsonConvert.SerializeObject(result, Formatting.Indented);
+                }
+                catch (Exception ex) 
+                {
+                    return BadRequest("Hubo un error en la linea de comandos indicada al motor de base de datos, excepción: " + ex.Message);
+                }
+
+
+                //return Json(result);    
+                return null;
+            }
+            
+            return null;
+        }
+
+        internal SqlConnection Connex(string ConnectionString)
+        {
+
+            return null;
+        }
+    }
+}
